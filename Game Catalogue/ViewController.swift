62b1031cd6ba49414048032d4a707742520f263e
Nanda Wawan Kurniawan <nandawawank@gmail.com>
2020-12-23 20:31:03 +0700

    Initial Commit

---
+++ Game Catalogue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game Catalogue
+//
+//  Created by Nanda Wawan Kurniawan on 23/12/20.
+//  Copyright © 2020 Dicoding Indoneisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
